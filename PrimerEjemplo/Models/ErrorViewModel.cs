6b1759c95f43d222eeb34a6cb153d9a7a094a650
Mateo Rosero <mateo.rosero@udla.edu.ec>
2023-09-25 08:28:43 -0500

    Agregar archivos de proyecto.

---
+++ PrimerEjemplo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrimerEjemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
